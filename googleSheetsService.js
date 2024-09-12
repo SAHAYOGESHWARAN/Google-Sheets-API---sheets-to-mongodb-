2ba641f25046eaa243c06203845a143fa8bd7eff
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-12 08:54:36 +0530

    GSA

---
+++ googleSheetsService.js
@@ -0,0 +1,32 @@
+const { google } = require('googleapis');
+const fs = require('fs');
+const TOKEN_PATH = 'token.json';
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+
+const authenticate = () => {
+    const credentials = JSON.parse(fs.readFileSync('credentials.json'));
+    const { client_secret, client_id, redirect_uris } = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+    // Check if we have previously stored a token.
+    if (fs.existsSync(TOKEN_PATH)) {
+        const token = fs.readFileSync(TOKEN_PATH);
+        oAuth2Client.setCredentials(JSON.parse(token));
+    } else {
+        // If not, guide the user to authorize
+        // Generate auth URL and handle token storage
+    }
+    return oAuth2Client;
+};
+
+const getGoogleSheetData = async (spreadsheetId, range) => {
+    const auth = authenticate();
+    const sheets = google.sheets({ version: 'v4', auth });
+    const res = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range,
+    });
+    return res.data.values;
+};
+
+module.exports = { getGoogleSheetData };
